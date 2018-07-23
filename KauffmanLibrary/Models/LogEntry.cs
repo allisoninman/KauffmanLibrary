2a6332149a920e5ccd764aca6f53b0999e814d2d
AllisonInman <ainman514@hotmail.com>
2018-07-22 20:51:00 -0700

    Fix date deserialization

--- KauffmanLibrary/Models/LogEntry.cs
+++ KauffmanLibrary/Models/LogEntry.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace KauffmanLibrary.Models
 {
     public class LogEntry
     {
         public string Name { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
         public DateTime Date { get; set; }
         public bool IsCheckingOut { get; set; }
     }
