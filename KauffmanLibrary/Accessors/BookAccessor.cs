1ad511fc0be7666d75386e0c6b70acf8e4d8606b
AllisonInman <ainman514@hotmail.com>
2018-07-22 21:19:43 -0700

    Remove unused using statements

--- KauffmanLibrary/Accessors/BookAccessor.cs
+++ KauffmanLibrary/Accessors/BookAccessor.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using KauffmanLibrary.Models;
 using MongoDB.Bson;
 using MongoDB.Driver;
--- KauffmanLibrary/Controllers/BookController.cs
+++ KauffmanLibrary/Controllers/BookController.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using KauffmanLibrary.Accessors;
 using KauffmanLibrary.Models;
-using Microsoft.AspNetCore.Http;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace KauffmanLibrary.Controllers
 { 
