37d58a43e6272c8964b166c4aadc349d0eee58f3
AllisonInman <ainman514@hotmail.com>
2018-07-22 17:18:11 -0700

    Update git ignore and add model classes

---
+++ KauffmanLibrary/Models/LogEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KauffmanLibrary.Models
+{
+    public class LogEntry
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public Boolean IsCheckingOut { get; set; }
+    }
+}
---
+++ KauffmanLibrary/Models/Rating.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KauffmanLibrary.Models
+{
+    public class Rating
+    {
+        public string Name { get; set; }
+        public int RatingValue { get; set; }
+    }
+}
