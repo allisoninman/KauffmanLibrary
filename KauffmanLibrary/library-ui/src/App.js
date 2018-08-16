e23c388c888a47fd866371c5890fa4c52ec4d4be
gauri <gauri.ramesh15@gmail.com>
2018-08-15 22:30:07 -0500

    removed logo

--- KauffmanLibrary/library-ui/src/App.js
+++ KauffmanLibrary/library-ui/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -47,7 +46,6 @@ receiveData(data) {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
